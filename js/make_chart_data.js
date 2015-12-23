2385b81451950360686ffaeb320606d1251ad90d
Lee Cunliffe <artbasher@gmail.com>
2015-12-23 14:19:04 -0800

    actuarial data and method to transform it

---
+++ js/make_chart_data.js
@@ -0,0 +1,17 @@
+/* globals actuaryData : true */
+/* exported getDataForAge */
+
+
+function getDataForAge(age) {
+  var output = [];
+  var livingFromStartToThisYear = 1;
+  for (var i = age; i < actuaryData.length; i++) {
+    var row = actuaryData[i];
+    var dyingThisYear = row[1];
+    var livingThisYear = 1 - dyingThisYear;
+    var dyingInThisYearFromStart = livingFromStartToThisYear * dyingThisYear;
+    livingFromStartToThisYear = livingFromStartToThisYear * livingThisYear;
+    output.push([i, dyingInThisYearFromStart]);
+  }
+  return output;
+}
